0c937b470f251bd12947192a9f525ddce989d3bf
Ajsela <AjsMuh19@htl-shkoder.com>
2026-03-18 11:07:26 +0100

    add basic authentication function

--- src/routes/api/restaurants/+server.js
+++ src/routes/api/restaurants/+server.js
@@ -1,6 +1,14 @@
 import pool from '$lib/server/database.js';
-
+''
 export async function GET() {
     const [rows] = await pool.query('SELECT * FROM restaurants');
     return Response.json(rows, { status: 200 });
+}
+
+function checkAuth(request) {
+    const auth = request.headers.get('authorization');
+    if (!auth || !auth.startsWith('Basic ')) return false;
+
+    const [user, pass] = atob(auth.split(' ')[1]).split(':');
+    return user === 'admin' && pass === 'albania2024';
 }
